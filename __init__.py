edcef2c2588262a3fc34ef812e21bc51b0b423b9
Tom Wu <qwu@microsoft.com>
2023-05-22 23:33:46 -0400

    add req

--- __init__.py
+++ __init__.py
@@ -1,9 +1,13 @@
 import logging
-
 import azure.functions as func
 
+app = func.FunctionApp()
 
-def main(myblob: func.InputStream):
-    logging.info(f"Python blob trigger function processed blob \n"
-                 f"Name: {myblob.name}\n"
-                 f"Blob Size: {myblob.length} bytes")
+@app.function_name(name="BlobTrigger1")
+@app.blob_trigger(arg_name="ndpi", 
+                  path="input",
+                  connection="https://ndpi.blob.core.windows.net/input")
+def test_function(ndpi: func.InputStream):
+   logging.info(f"Python blob trigger function processed blob \n"
+                f"Name: {ndpi.name}\n"
+                f"Blob Size: {ndpi.length} bytes")
